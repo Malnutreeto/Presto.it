5ea399edc5777f7a07f4df1954c9e5a39816cf6e
Antonio <antos176@gmail.com>
2023-03-29 00:55:13 +0200

    [Setup]Antonio: allineamento con gli altri branch

---
+++ database/migrations/2023_03_28_193316_create_sub_categories_main_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_categories_main_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('main_category_id');
+            $table->unsignedBigInteger('sub_category_id');  
+            $table->timestamps();
+
+            $table->foreign('main_category_id')->references('id')->on('main_categories');
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_categories_main_categories');
+    }
+};
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,64 @@
-<h1>Sono la register</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @vite (['resources/css/app.css', 'resources/js/app.js'])
+    
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6 mx-auto">
+                <h1>Register</h1>
+                <form action="/register" method="POST">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" id="name">
+                            @error('name')
+                            <span class="text-danger-small">Not valid</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <label for="surname">Surname</label>
+                            <input type="text" name="surname" class="form-control" id="surname">
+                            @error('surname')
+                            <span class="text-danger-small">Not valid</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <label for="nickname">Nickname</label>
+                            <input type="text" name="nickname" class="form-control" id="nickname">
+                            @error('nickname')
+                            <span class="text-danger-small">Not valid</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password">
+                            @error('password')
+                            <span class="text-danger-small">Not valid</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <label for="password_confirmation">Password Confirmation</label>
+                            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
+                            @error('password_confirmation')
+                            <span class="text-danger-small">Not valid</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div> 
+    </div>
+    
+</body>
+</html>

4b8c2f957f915af990b2b40dd6e34876d9308023
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-03-30 00:23:50 +0200

    [fix home blade]sperimentazione breakpoint per il responsive + icone

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,30 +1,32 @@
 
-
-<nav class="navbar navbar-expand-md bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Presto</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse " id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Utente
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Login</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Logout</a></li>
+<div class="container-fluid m-0 p-0">
+    <nav class="navbar navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Presto</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Utente
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Login</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+          </div>
+        </div>
+      </nav>
+
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,42 @@
 <x-layout>
-    <div class="container mx-1 h-100 d-flex flex-column"">
+    <div class="container-fluid mx-1  d-flex flex-column">
         <div class="row mt-2">
             <div class="col-lg-6 md-12 px-5">
                 <h1>Welcome to presto.it</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, possimus!</p>
             </div>
-            <div class="col-lg-6 md-12 ">
-                <img src="https://picsum.photos/500/300" class="rounded float-end" alt="">
+            <div class="col-lg-6 md-6 sm-12 d-flex justify-content-center">
+                <img src="https://picsum.photos/600/300" class="img-fluid" alt="Responsive image">
             </div>
         </div>
-        <div class="row mt-5 mx-1 ">
-            <div class="col-sm h-50 d-flex justify-content-center text-center border border-success p-1 m-1" >
-                <h3>
+        <div class="row mt-5 justify-content-between">
+            <div style="height: 170px; align-items: center; background-color: #1fba22c3;" class="col-lg-2 col-md-6 col-sm-12 d-flex flex-column justify-content-center text-center border border-success p-0 mx-2 m-1" >
+                <i class="bi bi-handbag fs-1"></i>
+                
+                <h5>
                     ABBIGLIAMENTO
-                </h3>
+                </h5>
             </div>
             
-            <div class="col-sm d-flex justify-content-center text-center border border-success p-1 m-1">
-                <h3>
+            <div style="height: 170px; align-items: center; background-color: #1fba22c3;" class="col-lg-2 col-md-6 col-sm-12 d-flex flex-column justify-content-center border border-success p-0 mx-2 m-1">
+                <i class="bi bi-scooter fs-1"></i>
+                <h5>
                     MOTORI
-                </h3>
+                </h5>
             </div>
             
-            <div class="col-sm d-flex justify-content-center text-center border border-success p-1 m-1">
-                <h3>
+            <div  style="height: 170px; align-items: center; background-color: #1fba22c3;" class="col-lg-2 col-md-6 col-sm-12 d-flex flex-column justify-content-center text-center border border-success p-0 mx-2 m-1">
+                <i class="bi bi-earbuds fs-1"></i>
+                <h5>
                     ELETTRONICA
-                </h3>
+                </h5>
             </div>
-            <div class="col-sm d-flex justify-content-center text-center border border-success p-1 m-1">
-                <h3>
+            <div style="height: 170px; align-items: center; background-color: #1fba22c3;" class="col-lg-2 col-md-6 col-sm-12 d-flex flex-column justify-content-center text-center border border-success p-0 mx-3 m-1">
+                <i class="bi bi-house-door fs-1"></i>
+
+                <h5>
                     CASA
-                </h3>
+                </h5>
             </div>
         </div>
     </div>

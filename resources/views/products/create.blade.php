ae80cda525093f9d229ea85658cc9ca7e19da1cd
frengo <flotito@disroot.org>
2023-03-30 12:10:00 +0200

    [resolve merge]

--- resources/views/products/create.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout>
-    <div class="container">
-        <div class="row"></div>
-            <div class="col-12">
-                <livewire:create-product />
-            </div>
-    </div>
-</x-layout>

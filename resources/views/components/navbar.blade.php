265b1cd85c23dbe8b894a396e952dd8f8689c0a4
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-03-31 19:30:41 +0200

    [navbar 2.0]modifiche in corso

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,23 +9,23 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
+          <li class="nav-item text-end">
             <a class="nav-link text-light active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item text-end">
             <a class="nav-link text-light" href="#">Link</a>
           </li>
-          <li class="nav-item dropdown">
+          <li class="nav-item dropdown text-end">
             <a class="nav-link text-light dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Utente
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/login">Login</a></li>
+            <ul class="dropdown-menu justify-content-start">
+              <li><a class="dropdown-item my-1" href="/login">Login</a></li>
               <li>
                 @auth
                 <li><hr class="dropdown-divider"></li>
-                <form class="dropdown-item" action="/logout" method="POST">
+                <form class="dropdown-item text-end" action="/logout" method="POST">
                   @csrf
                   <button type="submit">
                     Logout

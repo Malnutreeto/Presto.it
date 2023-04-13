d727a72a6548b59d34424fa35a1c2c82c16152b7
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-04-13 21:34:08 +0200

    [email/verify progress bar]

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,13 +1,24 @@
 <x-layout>
+    @vite('resources/css/mailreq.css')
     <div class="container">
+        <div class="container-bar">    
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2" >
+              </div>                       
+            </div> 
+          </div>
         <div class="row">
-            <div class="col-md-5 col-sm-12 mx-auto">
+            <div class="col-md-5 col-sm-12 mx-auto mt-4">
+                <img src="{{asset('mailreq.jpg')}}" alt="" width="350" height="300">
+                
+            </div>
+            <div class="col-md-5 col-sm-12 mx-auto mt-4">
                 <div class="card py-4">
                     <div class="card-body">
                         @if (session('status') == 'verification-link-sent')
                             <div class="alert alert-success text-center">A new email verification link has been emailed to you!</div>
                         @endif
-                        <div class="text-center mb-5">
+                        <div class="text-center mb-2">
                             <h3>Verify e-mail address</h3>
                             <p>You must verify your email address to access this page.</p>
                         </div>

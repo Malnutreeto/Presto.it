a7376b6e8bed086a4fb8387b6c71e40ffb5a547f
frengo <flotito@disroot.org>
2023-04-13 19:31:35 +0200

    [register captcha] cambiato il recaptcha da widget a invisible

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Presto.it - Registrati</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script>
+       function onSubmit(token) {
+         document.getElementById("demo-form").submit();
+       }
+     </script>
+
 </head>
 <body>
     @vite (['resources/css/app.css', 'resources/css/auth.css', 'resources/js/auth.js'])
-    
-    <form action="/register" method="POST">
+    <form action="/register" method="POST" id="demo-form">
     @csrf
     <div class="container split-background form-container mb-5">
         <img src="{{asset('titolo.png')}}" class="logo">
@@ -53,12 +59,12 @@
                 <div class="err-text">{{$message}}</div>
             @enderror
         </div>
-        <div class="main-div-field goggle" data-theme="light">
-            {!! NoCaptcha::renderJs() !!}
-            {!! NoCaptcha::display() !!}               
-        </div>
+        <!-- <div class="d-flex justify-content-center">
+            <div class="g-recaptcha" data-sitekey="6LdTkYMlAAAAAP6EdIQdR5bG2-m0NmgiReg-cKR4" data-theme="dark">
+            </div>               
+        </div> -->
         <div>
-            <button class="button">REGISTRATI</button>
+            <button class="button g-recaptcha" data-sitekey="6LcapoUlAAAAAG-tA3chcxsm0JfhXGk4ioDMTkVi" data-callback="onSubmit">REGISTRATI</button>
         </div>
     </div>
 </form>

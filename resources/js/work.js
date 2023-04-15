d39d3d2912815fd45e35ea37b4f3bef1823788a5
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-04-15 02:55:24 +0200

    [work with us frontend] fix effetto d-none con js + fix css

--- resources/js/work.js
+++ resources/js/work.js
@@ -1,18 +1,18 @@
 'use strict'
-
+console.log("cariato work");
 let container = document.querySelector('.container');
-let iconboxes = container.querySelectorAll('.iconbox')
+let iconboxes = container.querySelectorAll('.iconbox');
 
 container.addEventListener('mouseover', (e) => {
-     if(e.target.classList.contains('iconbox')) {
-        e.target.children[0].classList.toggle('d-none')
-        e.target.children[1].classList.toggle('d-none')
-     } else if (e.target.classList.contains('flex-wrap')) {
-        iconboxes.forEach((element=>{
-            element.children[0].classList.remove('d-none')
-            element.children[1].classList.add('d-none')
-        }))
-     }
+   if(e.target.classList.contains('iconbox')) { 
+      e.target.children[0].classList.add('d-none')
+      e.target.children[1].classList.remove('d-none')
+   } else if (e.target.classList.contains('row')) {
+      iconboxes.forEach((element=>{
+         element.children[0].classList.remove('d-none')
+         element.children[1].classList.add('d-none')
+      }))
+   }
 })
 
 
--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
     @vite(['resources/css/work.css', 'resources/js/work.js'])
     @if(session()->has('success'))
-        <div class="alert alert-success">
-            {{session('success')}}
-        </div>
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
     @endif
     <form class="" action="/work" method="POST" id="demo-form">
         @csrf
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12 d-flex justify-content-center">
-                    <button type="submit" class="submit btn btn-sm btn-outline-light btn-hover g-recaptcha" data-sitekey="6LcapoUlAAAAAG-tA3chcxsm0JfhXGk4ioDMTkVi" data-callback="onSubmit">
+                <div class="col-12 d-flex justify-content-center mt-5 mb-3">
+                    <button type="submit" class="submit send-request g-recaptcha" data-sitekey="6LcapoUlAAAAAG-tA3chcxsm0JfhXGk4ioDMTkVi" data-callback="onSubmit">
                         Invia la richiesta
                     </button>
                 </div>
@@ -20,11 +20,11 @@
     </form>
     {{-- <div class="bkg-img">
         <h1 class="text-center">Lavora con noi</h1>
-       
+        
         <div class="description">
             <h4>VUOI METTERTI IN GIOCO?</h4>
             <p>Offriamo la possibilità di iniziare un percorso di crescita professionale oppiure consolidare la carriera in un ambiente dinamico, giovane e all'avanguardia.</p>
-                  
+            
         </div>
         <div class="separation"></div>  
         <div class="how-to-work">
@@ -32,31 +32,29 @@
             <p>Clicca sul bottone qui in alto e i tuoi dati verranno automaticamente inviati ai nostri operatori. Riceverai una mail che confermerà che la tua richiesta è stata inviata correttamente</p>
         </div>
     </div> --}}
-  <div class="container d-flex justify-content-center flex-wrap mt-5">
-    <div class="iconbox">
-        <i class="bi bi-house-gear"></i>
-        <p class="">Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
+    <div class="container big-content">
+        <div class="row justify-content-center">
+            <div class=" col-4 iconbox">
+                <i class="bi bi-house-gear"></i>
+                <p class="d-none text-center">Hai sempre desiderato lavorare da casa? Ora puoi!</p>
+            </div>
+            <div class=" col-4 iconbox">
+                <i class="bi bi-clock"></i>
+                <p class="d-none text-center">Lavora senza limiti di orario!</p>
+            </div>
+        </div>
+        <div class="row justify-content-center" id="ib">
+            <div class="col-4 iconbox">
+                <i class="bi bi-emoji-laughing"></i>
+                <p class="d-none text-center">Portaci degli amici, diventeranno tuoi colleghi!</p>
+            </div>
+            <div class="col-4 iconbox">
+                <i class="bi bi-award"></i>
+                <p class="d-none text-center">Fai carriera rapidamente!</p>
+            </div>
+        </div>
     </div>
-    <div class="iconbox">
-        <i class="bi bi-clock"></i>
-        <p class="d-none">Lorem ipsum dolor sit amet.</p>
-    </div>
-    <div class="iconbox">
-        <i class="bi bi-emoji-laughing"></i>
-        <p class="d-none">Lorem ipsum dolor sit amet.</p>
-    </div>
-    <div class="iconbox">
-        <i class="bi bi-award-fill"></i>
-        <p class="d-none">Lorem ipsum dolor sit amet.</p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-3">
-        <p>Desideri lavorare in smart? Ora puoi!</p>
-    </div>
-    <div class="col-3"></div>
-    <div class="col-3"></div>
-    <div class="col-3"></div>
-  </div>
+    
+    
+    
 </x-layout>

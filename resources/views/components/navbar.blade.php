7fcb18839ea2cc4305ed12e94554004e3128f0e7
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-04-17 17:17:11 +0200

    allineamento

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,75 +1,55 @@
-@vite('resources/css/navbar.css')
-{{-- 
-<nav class="navbar row navbar-expand-sm bg-body-tertiary">
-  <div class="container-fluid col-4">
+@vite(['resources/css/navbar.css', 'resources/js/navbar.js'])
+
+
+
+<header id="logo" class="header">
+  <div id="header-logo" class="logo">
+    <img src="{{'conbusta.png'}}" alt="" width="150" height="140">
+    <div id="scritte-logo" class="scritte-logo" >
+        <p id="compra" class="compra" >Compra.</p>
+        <p id="vendi" class="vendi">Vendi.</p>
+        <p id="presto" class="presto">Presto.</p>
+    </div>
     
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav row  mb-2 mb-lg-0">
-        <li class="col-4">
-          @if(auth()->user() && auth()->user()->email_verified_at)
-          <a href="{{route('product.create')}}" class="text-decoration-none text-light ">
-            <button class="btn btn-sm ">
-              Inserisci annuncio
-            </button>
-          </a>
-          <li class="nav-item text-end mt-1">
-            <a class="nav-link text-light" href="/work">Lavora con noi</a>
-          </li>
-          @endif
-        </li>
-      </ul>
-     </div>
   </div>
-  <div class="col-4">
-    <a class="navbar-brand" href="/">
-      <img src="{{ asset('titolo.png') }}" alt="" width="150" height="40">
-    </a>
+ 
+</header>
+
+
+<div id="navbar" class="nav">
+  <input type="checkbox" id="nav-check">
+  <div class="nav-header">
+    <div id="nav-logo" class="nav-logo">
+      <a href="/"><img src="{{ asset('solobusta.png') }}" alt="" width="50" height="40"></a>
+    </div>
   </div>
-  <div class="col-4">
-    <li class="nav-item text-end mt-1">
-      <a class="nav-link text-light" href="/category">Categorie</a>
-    </li>
-    <li class="nav-item text-end mt-1">
-      <a class="nav-link text-light" href="/product">Prodotti</a>
-    </li>
-    @auth
-    <li class="nav-item dropdown text-end mt-1">
-      <a class="nav-link text-light dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        {{auth()->user()->nickname}}
-      </a>
-      <ul class="dropdown-menu p-0 dropdown-menu-end ">
-        @if(auth()->user()->role_id < 4)
-        <li class="">
-          <a class="nav-link text-light p-0 ms-1" href="/admin">Pannello di controllo</a>
-        </li>
-        @endif
-        <li class= "dropdown-item text-end p-0 ps-sm-0 logout-field w-100">
-          
-          <form  class="" action="/logout" method="POST">
-            @csrf
-            <button type="submit" class="submit btn btn-sm logout-button">
-              Logout
-            </button>
-          </form>
-          
-          @else
-          <li class="nav-item text-end mt-1">
-            <a class="nav-link text-light" href="/login">Login</a>
-          </li>
-          
-          @endauth
-          
-        </li>
-      </ul>
-    </li>
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
   </div>
   
-  
-</nav> --}}
-<div class="container-fluid m-0 p-0">
+  <div class="nav-links">
+    {{-- <ul>
+      <li>
+        @if(auth()->user() && auth()->user()->email_verified_at)
+              <button class="btn btn-sm btn-outline-light btn-hover">
+                <strong><a href="{{route('product.create')}}" class="text-decoration-none text-light ">Inserisci annuncio</a></strong>
+              </button>
+            @endif
+      </li>
+    </ul> --}}
+    <a href="http://stackoverflow.com/users/4084003/" target="_blank">inserisci annuncio</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">categorie</a>
+    
+    <a href="https://codepen.io/jo_Geek/" target="_blank">prodotti</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">utente</a>
+  </div>
+</div>
+
+{{-- <div class="container-fluid m-0 p-0">
   <nav class="navbar navbar-expand-md bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand text-light" href="/">
@@ -82,50 +62,37 @@
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
           <li class="d-flex align-items-end justify-content-end button-end">
             @if(auth()->user() && auth()->user()->email_verified_at)
-            <a href="{{route('product.create')}}" class="text-decoration-none text-light ">
-              <button class="btn btn-sm wwu-button">
-                Inserisci annuncio
+              <button class="btn btn-sm btn-outline-light btn-hover">
+                <strong><a href="{{route('product.create')}}" class="text-decoration-none text-light ">Inserisci annuncio</a></strong>
               </button>
-            </a>
-            <li class="nav-item text-end mt-1">
-              <a class="nav-link text-light" href="/work">Lavora con noi</a>
-            </li>
             @endif
-          </li>
-          
-          <li class="nav-item text-end mt-1">
+            </li>
+          <li class="nav-item text-end mt-1 hover-overlay">
             <a class="nav-link text-light" href="/category">Categorie</a>
           </li>
           <li class="nav-item text-end mt-1">
-            <a class="nav-link text-light" href="/product">Prodotti</a>
+            <a class="nav-link text-light" href="/category">Prodotti</a>
           </li>
           @auth
           <li class="nav-item dropdown text-end mt-1">
             <a class="nav-link text-light dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               {{auth()->user()->nickname}}
             </a>
-            <ul class="dropdown-menu p-0 dropdown-menu-end ">
-              @if(auth()->user()->role_id < 4)
-              <li class="">
-                <a class="nav-link text-light p-0 ms-1" href="/admin">Pannello di controllo</a>
-              </li>
-              @endif
-              <li class= "dropdown-item text-end p-0 ps-sm-0 logout-field w-100">
-                
-                <form  class="" action="/logout" method="POST">
+            <ul class="dropdown-menu p-0 ml-">
+              <li>
+                <form class="dropdown-item text-end p-0 ps-sm-0 logout-field" action="/logout" method="POST">
                   @csrf
-                  <button type="submit" class="submit btn btn-sm logout-button">
+                  <button type="submit" class="submit btn btn-sm btn-outline-light btn-hover">
                     Logout
                   </button>
                 </form>
-                
                 @else
                 <li class="nav-item text-end mt-1">
                   <a class="nav-link text-light" href="/login">Login</a>
                 </li>
-                
+              
                 @endauth
-                
+
               </li>
             </ul>
           </li>
@@ -133,4 +100,4 @@
       </div>
     </div>
   </nav>
-</div>
+</div> --}}

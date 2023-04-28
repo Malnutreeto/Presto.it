0b72552428b1b098383ad39870a818683d0bb72a
“Malnutreeto” <emanuele.tritone@gmail.com>
2023-04-28 22:36:30 +0200

    [footer fe]

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,8 @@
 <div class="footer footer-link container-fluid align-items-center">
     <div class="row h-100">
         <div class="col-1">
-            <a href="/" > <img src="{{asset('site_img/solobusta.png')}}" class="logo-footer img-fluid mt-lg-0 mt-sm-2" alt=""></div></a>
+           <img src="{{asset('site_img/solobusta.png')}}" class="logo-footer " alt=""></div>
                
-        <div class="col-11 m-0 d-flex justify-content-end align-items-center"><p class="p-footer m-0">&copy Foreach Project 2023 Antonio - Emanuele - Francesco</p><</div>
+        <div class="col-lg-11 col-md-8 col-sm-12 m-0 d-flex justify-content-lg-end justify-content-sm-center align-items-center"><p class="p-footer m-0">&copy Foreach Project 2023 Antonio - Emanuele - Francesco</p><</div>
     </div>
 </div>
